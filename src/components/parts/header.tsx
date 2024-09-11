1ebbce39162c4d9078709a679e30fa128dfb9bd2
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-12 00:18:03 +0900

    feat: header下に説明文を追加

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -1,7 +1,12 @@
 export const Header = (): React.ReactNode => {
   return (
     <header className="sticky top-0 z-50 py-4 px-8 flex gap-4 items-center justify-between bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
-      <p className="text-2xl font-bold">Contrast Color Picker</p>
+      <div className="flex flex-col gap-2">
+        <p className="text-2xl font-bold">Contrast Color Picker</p>
+        <p className="text-sm text-muted-foreground">
+          <em>oklch()</em> + lightness curve / color palette for dark theme as well as light theme
+        </p>
+      </div>
     </header>
   );
 };
